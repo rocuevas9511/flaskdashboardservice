3b293f531cbc3ee029aaac3c56569caf84afc316
Felipe Diaz <luisfelipe.diazchica@honeywell.com>
2019-06-18 14:59:14 -0500

    return 210 results with 3 combination values

--- services/satisfaction.js
+++ services/satisfaction.js
@@ -2,22 +2,26 @@
 const connect = require('../db/db')
 const DBNAME = "globaldb";
 const TABLE_NAME = "Metrics";
-const getAllSatisfactions =  (req, res) => {
-  connect( async db => {
+const getAllSatisfactions = (req, res) => {
+  connect(async db => {
     var dbo = db.db(DBNAME);
-    var sentimentResults = await dbo.collection(TABLE_NAME).find({ "Metric": "AVG Text Sentiment"})
-      .limit(100)
-      .sort({CalculationDate : -1})
+    var sentimentResults = await dbo.collection(TABLE_NAME).find({ "Metric": "AVG Text Sentiment" })
+      .limit(70)
+      .sort({ CalculationDate: -1 })
       .toArray();
 
-    var imageResults = await dbo.collection(TABLE_NAME).find({ "Metric": "Facial Expression Count"})
-      .limit(100)
-      .sort({CalculationDate : -1})
+    var imageResults = await dbo.collection(TABLE_NAME).find({ "Metric": "Facial Expression Rate" })
+      .limit(70)
+      .sort({ CalculationDate: -1 })
       .toArray()
+
+    var imageResults = await dbo.collection(TABLE_NAME).find({ "Metric": "Facial Expression Count" })
+      .limit(70)
+      .sort({ CalculationDate: -1 })
+      .toArray()
+    res.send(sentimentResults.concat(imageResults))
   
-      res.send(sentimentResults.concat(imageResults))
-      db.close();
-    
+    db.close();
   })
 }
 
@@ -27,14 +31,14 @@ const getSatisfactionByRange = (req, res, params) => {
     let { from, to } = params;
 
     from = (new Date(from)).toISOString();
-    to  = (new Date(to)).toISOString();
+    to = (new Date(to)).toISOString();
     dbo.collection(TABLE_NAME).find({
       date: {
         $gte: from,
         $lt: to
       }
-    }).toArray( (err, result) => {
-      if(err){
+    }).toArray((err, result) => {
+      if (err) {
         throw err;
       }
       db.close()
