4042bf418a2aca221c2483d26d4693b543773a0b
Felipe Diaz <luisfelipe.diazchica@honeywell.com>
2019-06-17 22:54:38 -0500

    Change main port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 var MongoClient = require('mongodb').MongoClient;
 const password = encodeURIComponent("zhQkmspqWuq3EHVVO8ht5kESJgxgIoDHjrjzW8tgXizjYjXxdI889SKlbA3hngkS9CXZ8kWzNGTzhgd88t8fiQ==")
 var url =  `mongodb://dbcarebot:${password}@dbcarebot.documents.azure.com:10255/?ssl=true&replicaSet=globaldb`
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Example app listening on port ${process.env.PORT || 3000}!`),
+app.listen(process.env.PORT || 1337, () =>
+  console.log(`Example app listening on port ${process.env.PORT || 1337}!`),
 );
 
 app.get('/', function (req, res) {
